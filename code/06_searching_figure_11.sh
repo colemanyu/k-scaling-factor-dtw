22ed080f34b1fc956d2ddf5fc762c8d55a0ef65b
cyuab <cyuab@connect.ust.hk>
2026-03-30 21:26:16 +0900

    Refactor experiment execution order in 06_searching_figure_11.sh

--- code/06_searching_figure_11.sh
+++ code/06_searching_figure_11.sh
@@ -7,14 +7,26 @@ l_values=(1.25 1.50 1.75 2.00)
 # l_values=(1.50)
 dist_methods=(0 1)
 functions_to_run=("psdtw_prime_parallel_bsf" "psdtw_prime_parallel_bsf_lb" "psdtw_prime_parallel_bsf_lb2")
-# functions_to_run=("psdtw_prime_parallel_bsf_lb3")
 
-for function_used in "${functions_to_run[@]}"; do
-    echo "Starting experiments for function: $function_used"
+# for function_used in "${functions_to_run[@]}"; do
+#     echo "Starting experiments for function: $function_used"
 
-    for P in "${P_values[@]}"; do
-        for l in "${l_values[@]}"; do
-            for dist_method in "${dist_methods[@]}"; do
+#     for P in "${P_values[@]}"; do
+#         for l in "${l_values[@]}"; do
+#             for dist_method in "${dist_methods[@]}"; do
+#                 echo "--------------------------------------------------"
+#                 echo "Running with P=$P, l=$l, dist_method=$dist_method"
+#                 python 06_searching_figure_11.py --P "$P" --l "$l" --function_used "$function_used" --dist_method "$dist_method"
+#             done
+#         done
+#     done
+# done
+
+for P in "${P_values[@]}"; do
+    for l in "${l_values[@]}"; do
+        for dist_method in "${dist_methods[@]}"; do
+            for function_used in "${functions_to_run[@]}"; do
+                echo "Starting experiments for function: $function_used"
                 echo "--------------------------------------------------"
                 echo "Running with P=$P, l=$l, dist_method=$dist_method"
                 python 06_searching_figure_11.py --P "$P" --l "$l" --function_used "$function_used" --dist_method "$dist_method"
@@ -22,5 +34,6 @@ for function_used in "${functions_to_run[@]}"; do
         done
     done
 done
+
 
 echo "All experiments completed."
