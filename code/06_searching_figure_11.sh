3c0460f252fca5275003fadf06eaac96e34aa35e
cyuab <cyuab@connect.ust.hk>
2026-03-31 02:19:51 +0900

    Update Figure 11

--- code/06_searching_figure_11.sh
+++ code/06_searching_figure_11.sh
@@ -6,21 +6,7 @@ P_values=(2 3 4)
 l_values=(1.25 1.50 1.75 2.00)
 # l_values=(1.50)
 dist_methods=(0 1)
-functions_to_run=("psdtw_prime_parallel_bsf" "psdtw_prime_parallel_bsf_lb2" "psdtw_prime_parallel_bsf_lb3")
-
-# for function_used in "${functions_to_run[@]}"; do
-#     echo "Starting experiments for function: $function_used"
-
-#     for P in "${P_values[@]}"; do
-#         for l in "${l_values[@]}"; do
-#             for dist_method in "${dist_methods[@]}"; do
-#                 echo "--------------------------------------------------"
-#                 echo "Running with P=$P, l=$l, dist_method=$dist_method"
-#                 python 06_searching_figure_11.py --P "$P" --l "$l" --function_used "$function_used" --dist_method "$dist_method"
-#             done
-#         done
-#     done
-# done
+functions_to_run=("psdtw_prime_parallel_bsf" "psdtw_prime_parallel_bsf_lb2")
 
 for P in "${P_values[@]}"; do
     for l in "${l_values[@]}"; do
